9719d2fc1eb8f55c9857f22c67c2022e98b83d4f
avocadocodes <nikitakar82925@gmail.com>
2024-04-05 14:33:19 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components"; 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import Tracker from "./components/Tracker"; 
+
+import GlobalStyles from "./globalStyles"; 
+
+  
+const Main = styled.div` 
+
+  display: flex; 
+
+  justify-content: center; 
+
+  align-items: center; 
+`; 
+
+  
+const App = () => { 
+
+  return ( 
+
+    <Main> 
+
+      <GlobalStyles /> 
+
+      <Tracker /> 
+
+    </Main> 
+
+  ) 
+} 
+
+  
 
 export default App;
